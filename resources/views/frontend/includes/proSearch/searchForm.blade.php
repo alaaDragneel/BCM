dc5f4121841289530f257820c3f7dad3c4a9e3f9
alaaDragneel <moaalaa16@gmail.com>
2016-11-24 14:42:15 +0200

    fix

--- resources/views/frontend/includes/proSearch/searchForm.blade.php
+++ resources/views/frontend/includes/proSearch/searchForm.blade.php
@@ -1,6 +1,5 @@
-
-        <input type="text" class="form-control searchBox" name="search" placeholder="Search for the Partners " id="search" required autocomplete="off">
-    <div class="back">
-         <span class="btn btn-danger">&times;</span>
-         <div class="results searchAjax"></div>
-    </div>
+<input type="text" class="form-control searchBox" name="search" placeholder="Search for the Partners " id="search" required autocomplete="off">
+<div class="back">
+  <span class="btn btn-danger">&times;</span>
+  <div class="results searchAjax"></div>
+</div>
