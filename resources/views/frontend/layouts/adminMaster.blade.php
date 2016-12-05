92d4e79ab552fbc2306c611a2d1b1f31eed66908
alaaDragneel <moaalaa16@gmail.com>
2016-12-05 11:51:39 +0200

    prepare for main info part 2

--- resources/views/frontend/layouts/adminMaster.blade.php
+++ resources/views/frontend/layouts/adminMaster.blade.php
@@ -36,24 +36,13 @@
      <div class="main-info">
           <div class="row">
                <div class="col-md-4">
-                    <div class="col-md-12">
-
-                    </div>
-                    <div class="col-md-12 dashboard">
-
-                    </div>
-                    <div class="col-md-12 projects">
-
-                    </div>
-                    <div class="col-md-12 teamwork">
-
-                    </div>
-                    <div class="col-md-12 rateMember">
-
-                    </div>
-                    <div class="col-md-12 quick">
-
-                    </div>
+                    <div class="col-md-12"></div>
+                    <div class="col-md-12"></div>
+                    <div class="col-md-12"></div>
+                    <div class="col-md-12 teamwork"></div>
+                    <div class="col-md-12 rateMember"></div>
+                    <div class="col-md-12 quick"></div>
+                    <div class="col-md-12 back-div"></div>
                </div>
                <div class="col-md-8">
 
